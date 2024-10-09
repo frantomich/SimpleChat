0680e1ad0f7bcb7eb8d3af1cd3f3eb832400fca0
Franklin L. Tomich <134819642+frantomich@users.noreply.github.com>
2024-10-09 14:45:13 -0300

    Server polishing and bug fixing

--- server.py
+++ server.py
@@ -4,62 +4,91 @@ import threading #Biblioteca para gerenciamento de threads.
 HOST = '127.0.0.1' #Endereço IP do servidor.
 PORT = 65432 #Porta de comunicação do servidor.
 
-clients = dict() #Armazena os endereços IP dos clientes.
-connections = dict() #Armazena as conexões com os clientes.
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Define o tipo de conexão usada pelo servidor (IPv4, TCP).
+CLIENTS = dict() #Armazena o endereço IP e porta dos clientes.
+CONNECTIONS = dict() #Armazena as conexões com os clientes.
 
 def main():
 
+    """Função principal do servidor."""
+
+    srv = threading.Thread(target=init_server, daemon=True)
+    srv.start()
+
+    cmd = threading.Thread(target=prompt, daemon=True)
+    cmd.start()
+
+    cmd.join()
+    exit(0)
+
+def init_server():
+
     """Inicia o servidor e aguarda a conexão de clientes."""
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        server.bind((HOST, PORT))
-        server.listen()
-        print(f"Servidor escutando em {HOST}:{PORT}")
+        SERVER.bind((HOST, PORT))
+        SERVER.listen()
+        print(f"Servidor escutando em {HOST}:{PORT}\n")
     except:
         print("Não foi possível iniciar o servidor!")
-        exit()
+        SERVER.close()
+        exit(0)
     else:
         while True:
-            conn, addr = server.accept()
-            threading.Thread(target=handle_client, args=(conn, addr)).start()
-    finally:
-        server.close()
-        exit()
+            conn, addr = SERVER.accept()
+            threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
 
 def handle_client(conn, addr):
 
-    """Estabelece a conexão com o cliente."""
+    """Estabelece a conexão com os clientes."""
 
-    try:
-        message = conn.recv(1024).decode('utf-8')
-        if message.startswith("<client>"):
-            message = message.replace("<client>", "")
-            message = message.split(":")
-            print(message)
-            username = message[0]
-        clients[username] = (message[1], int(message[2]))
-        connections[username] = conn
-        print(f"Conexão estabelecida com @{username} ¬> {addr[0]}:{addr[1]}")
-        conn.send(f"<info>Conexão com o servidor estabelecida com sucesso!".encode('utf-8'))
-    except:
-        print("Erro ao estabelecer conexão com o cliente!")
-        conn.close()
+    message = conn.recv(1024).decode('utf-8')
+    if message.startswith("<client>"):
+        username, ip, port = message.replace("<client>", "").split(":")
+    CLIENTS[username] = (ip, int(port))
+    CONNECTIONS[username] = conn
+    print(f"Conexão estabelecida com {username} ¬> {addr[0]}:{addr[1]}\n")
+    conn.send(f"<pass>".encode('utf-8'))
+    for c in CLIENTS:
+        send_online_users(c)
     while True:
-        try:
-            message = conn.recv(1024).decode('utf-8')
-            if message.startswith("$update"):
-                online_users = clients.copy()
-                del online_users[username]
-                connections[username].send(f"<online_users>{str(online_users)}".encode('utf-8'))
-            if message.startswith("$exit"):
-                break
-        except:
+        message = conn.recv(1024).decode('utf-8')
+        if message.startswith("$update"):
+            send_online_users(username)
+        if message.startswith("$exit"):
             break
-    print(f"Usuário @{username} desconectado!")
-    del clients[username]
-    del connections[username]
+    print(f"Usuário {username} desconectado!\n")
+    del CLIENTS[username]
+    del CONNECTIONS[username]
     conn.close()
+
+def send_online_users(username):
+
+    """Envia a lista de usuários online para o cliente."""
+
+    online_users = CLIENTS.copy()
+    del online_users[username]
+    CONNECTIONS[username].send(f"<online_users>{online_users}".encode('utf-8'))
+
+def prompt():
+
+    """Exibe o prompt do servidor."""
+
+    while True:
+        command = input()
+        if command.startswith("exit"):
+            for conn in CONNECTIONS:
+                print("oi")
+                conn.send(f"<exit>".encode('utf-8'))
+            SERVER.close()
+            break
+        elif command.startswith("users"):
+            print("Usuários online: ")
+            for user in CLIENTS:
+                print(f"{user} ¬> {CLIENTS[user][0]}:{CLIENTS[user][1]}")
+            print("\n")
+        else:
+            print("Comando inválido!\n")
 
 if __name__ == "__main__":
     main()
