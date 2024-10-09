22a38424d4b159e88495cd0bdaa2c0541d5a43f8
Franklin L. Tomich <134819642+frantomich@users.noreply.github.com>
2024-10-09 02:05:36 -0300

    Client to client communication implemented

--- alt/client.py
+++ alt/client.py
@@ -1,60 +1,156 @@
 import socket #Biblioteca para comunicação em rede.
 import threading #Biblioteca para gerenciamento de threads.
-import subprocess #Biblioteca para execução de comandos do sistema.
 
-HOST = '127.0.0.1' #Endereço IP do servidor.
-PORT = 65432 #Porta de comunicação do servidor.
+SERVER_ADDR = '127.0.0.1' #Endereço IP do servidor.
+SERVER_PORT = 65432 #Porta de comunicação do servidor.
+CLIENT_ADDR = '127.0.0.1' #Endereço IP do cliente.
+CLIENT_PORT = 65433 #Porta de comunicação do cliente.
 
-online_users = dict() #Armazena os nomes de usuário e endereços IP dos clientes.
+client_username = str() #Armazena o nome de usuário do cliente.
+client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Armazena a conexão do cliente com o servidor.
+
+online_users = dict() #Armazena os endereços dos clientes que estiverem online.
+online_connections = dict() #Armazena as conexões com os clientes que estiverem online.
 
 def main():
+     
+    connect_to_server()
+    threading.Thread(target=receive_connections, daemon=True).start()
+    send_messages()
+    for conn in online_connections.values():
+        conn.close()
+    client_connection.close()
+    exit()
+
+def connect_to_server():
 
     """Inicia o cliente e estabelece a conexão com o servidor."""
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    global client_username, client_connection, online_users, online_connections
+
     try:
-        client.connect((HOST, PORT))
-        print(f"Conectado ao servidor {HOST}:{PORT}")
-        username = input("Digite o seu nome de usuário: ")
-        client.send(username.encode('utf-8'))
-        print(client.recv(1024).decode('utf-8'))
+        client_connection.connect((SERVER_ADDR, SERVER_PORT))
+        print(f"Conectado ao servidor {SERVER_ADDR}:{SERVER_PORT}")
+        client_username = input("Digite o seu nome de usuário: ")
+        client_connection.send(f"<client>{client_username}:{CLIENT_ADDR}:{CLIENT_PORT}".encode('utf-8'))
+        message = client_connection.recv(1024).decode('utf-8')
+        if message.startswith("<info>"):
+            print(message.replace("<info>", ""))
     except:
         print("Não foi possível estabelecer conexão com o servidor!")
         exit()
     else:
-        threading.Thread(target=receive_messages_from_server, args=(client,), daemon=True).start()
-        send_messages_to_server(client)
-    finally:
-        client.close()
+        threading.Thread(target=receive_messages_from_server, args=(client_connection,), daemon=True).start()
+
+def receive_connections():
+        
+    """Recebe conexões de outros clientes."""
+
+    global client_username, client_connection, online_users, online_connections
+
+    receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        receiver.bind((CLIENT_ADDR, CLIENT_PORT))
+        receiver.listen()
+        print(f"Recepção de usuários iniciada em {CLIENT_ADDR}:{CLIENT_PORT}")
+    except:
+        print("Não foi possível iniciar a recepção de usuários!")
         exit()
+    else:
+        while True:
+            conn, addr = receiver.accept()
+            message = conn.recv(1024).decode('utf-8')
+            if message.startswith("<client>"):
+                username, ip, port = message.replace("<client>", "").split(":")
+                if username in online_users.keys():
+                    online_connections[username] = conn
+                    threading.Thread(target=receive_messages_from_client, args=(username,), daemon=True).start()
+                    conn.send("<pass>".encode('utf-8'))
+                else:
+                    conn.send("<erro>".encode('utf-8'))
+                    conn.close()
+    finally:
+        receiver.close()
+
+def connect_to_other_client(username):
+    
+        """Conecta-se a outro cliente."""
+
+        global client_username, client_connection, online_users, online_connections
+    
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        try:
+            client.connect((online_users[username][0], online_users[username][1]))
+            client.send(f"<client>{client_username}:{CLIENT_ADDR}:{CLIENT_PORT}".encode('utf-8'))
+            message = client.recv(1024).decode('utf-8')
+            if message.startswith("<erro>"):
+                return False
+            print(f"@{username} conectado!")
+            online_connections[username] = client
+        except:
+             return False
+        else:
+            threading.Thread(target=receive_messages_from_client, args=(username,), daemon=True).start()
+            return True
 
 def receive_messages_from_server(client):
     
         """Recebe mensagens do servidor."""
+
+        global client_username, client_connection, online_users, online_connections
     
         while True:
             try:
                 message = client.recv(1024).decode('utf-8')
                 if message.startswith("<online_users>"):
                     online_users = dict(eval(message.replace("<online_users>", "")))
-                    print(f"Usuários online: {', '.join(online_users.keys())}")
+                    if len(online_users) > 0:
+                        print(f"Usuários online: {', '.join(online_users.keys())}")
+                    else:
+                        print("Nenhum outro usuário online!")
                 else:
                     print(message)
             except:
                 break
 
-def send_messages_to_server(client):
+def receive_messages_from_client(username):
+        
+        global client_username, client_connection, online_users, online_connections
     
-        """Envia mensagens para o servidor."""
+        """Recebe mensagens de outros clientes."""
     
         while True:
-            message = input("¬>")
-            if message.startswith("@"): 
-                """ao digitar @usuário, deverá abrir um novo terminal para conversa privada com o usuário especificado."""
-                pass
-            client.send(message.encode('utf-8'))
-            if message == "exit":
+            try:
+                message = online_connections[username].recv(1024).decode('utf-8')
+                if message:
+                    print(f"<{username}>: {message}\n")
+            except:
                 break
+
+def send_messages():
+    
+    """Envia mensagens para o servidor ou outros clientes."""
+
+    global client_username, client_connection, online_users, online_connections
+
+    while True:
+        message = input("¬>")
+        if message.startswith("$"):
+            client_connection.send(message.encode('utf-8'))
+            if message.startswith("$exit"):
+                print("Desconectando...")
+                break
+        if message.startswith("@"):
+            is_online = True
+            username = message.split()[0][1:]
+            username = username.replace("@", "")
+            if username not in online_connections.keys():
+                is_online = connect_to_other_client(username)
+            if is_online:
+                online_connections[username].send(message.encode('utf-8'))
+            else:
+                print(f"Não foi possível conectar-se a @{username}!")
 
 if __name__ == '__main__':
     main()
--- alt/server.py
+++ alt/server.py
@@ -4,7 +4,7 @@ import threading #Biblioteca para gerenciamento de threads.
 HOST = '127.0.0.1' #Endereço IP do servidor.
 PORT = 65432 #Porta de comunicação do servidor.
 
-clients = dict() #Armazena os nomes de usuário e endereços IP dos clientes.
+clients = dict() #Armazena os endereços IP dos clientes.
 connections = dict() #Armazena as conexões com os clientes.
 
 def main():
@@ -32,12 +32,16 @@ def handle_client(conn, addr):
     """Estabelece a conexão com o cliente."""
 
     try:
-        username = conn.recv(1024).decode('utf-8')
-        clients[username] = addr
+        message = conn.recv(1024).decode('utf-8')
+        if message.startswith("<client>"):
+            message = message.replace("<client>", "")
+            message = message.split(":")
+            print(message)
+            username = message[0]
+        clients[username] = (message[1], int(message[2]))
         connections[username] = conn
         print(f"Conexão estabelecida com @{username} ¬> {addr[0]}:{addr[1]}")
-        conn.send("Conexão estabelecida com sucesso!".encode('utf-8'))
-        send_online_users(username)
+        conn.send(f"<info>Conexão com o servidor estabelecida com sucesso!".encode('utf-8'))
     except:
         print("Erro ao estabelecer conexão com o cliente!")
         conn.close()
@@ -45,7 +49,9 @@ def handle_client(conn, addr):
         try:
             message = conn.recv(1024).decode('utf-8')
             if message.startswith("$update"):
-                send_online_users(username)
+                online_users = clients.copy()
+                del online_users[username]
+                connections[username].send(f"<online_users>{str(online_users)}".encode('utf-8'))
             if message.startswith("$exit"):
                 break
         except:
@@ -54,14 +60,6 @@ def handle_client(conn, addr):
     del clients[username]
     del connections[username]
     conn.close()
-
-def send_online_users(username):
-
-    """Envia a lista de usuários online para o cliente."""
-
-    online_users = clients.copy()
-    del online_users[username]
-    connections[username].send(f"<online_users>{str(online_users)}".encode('utf-8'))
 
 if __name__ == "__main__":
     main()
